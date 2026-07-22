34c8c6bb5fced5899bf146775ae13cc82ca452be
ShumOliv <shumir.oliver@gmail.com>
2026-07-22 20:26:06 +0530

    Implement weather check and Twilio notification
    Fetch weather data and send a WhatsApp message if rain is expected.

---
+++ main.py
@@ -0,0 +1,45 @@
+# import os
+import requests
+from twilio.rest import Client
+# from twilio.http.http_client import TwilioHttpClient
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+# Twilio authentication
+account_sid = os.environ.get("ACCCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+
+
+weather_params = {
+    "lat": 19.075983,
+    "lon": 72.877655,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+print(response.status_code)
+# print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    # print(hour_data["weather"][0]["id"])
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+       # print("Carry your umbrella")
+    #   proxy_client = TwilioHttpClient ()
+    #   proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+       client = Client(account_sid, auth_token)
+       message = client.messages.create(
+           body="It's going to rain today. Remember to bring an umbrella!☂️",
+           from_="whatsapp:+14155238886",
+           to="whatsapp:+917042250019",
+       )
+       print(message.status)
+       # print(message.body)
